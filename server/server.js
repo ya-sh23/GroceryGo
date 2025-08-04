7415e39ecd3b7fab56955530b198ec3a663f6571
ya-sh23 <yashpatel231203@gmail.com>
2025-08-05 00:19:26 +0530

    added origins

--- server/server.js
+++ server/server.js
@@ -19,7 +19,10 @@ await connectDB();
 await connectCloudinary();
 
 // Allow multiple origins
-const allowedOrigins = ["http://localhost:5173"];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://grocerygo-blue.vercel.app",
+];
 
 app.post('/stripe',express.raw({type: 'application/json'}), stripeWebhooks)
 
